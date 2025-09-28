1a8df9e9f25a14b01776ce5b015f8a2fc3e35d7b
Hyperboid <23138363+Hyperboid@users.noreply.github.com>
2025-09-28 11:27:18 -0400

    Shared gonerbg fix

--- scripts/world/maps/device.lua
+++ scripts/world/maps/device.lua
@@ -5,6 +5,10 @@ function map:init(world,data)
 end
 
 function map:onEnter()
+    if Mod.shared_gonerbg then
+        Mod.shared_gonerbg:remove()
+        Mod.shared_gonerbg = nil
+    end
     love.audio.newSource(Assets.getMusicPath"DEVICE_ON", "static"):play()
     self.world:startCutscene("device-menu.main")
     self.world.timer:after(2, function ()
